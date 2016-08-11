b5ca02128acbc56eed73d735fd1f56234e769266
Francis Prus and Jeremy Fryd <francis@>
2016-08-11 14:24:47 -0700

    Add operational js functions and completed html>

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,65 @@
-//var number1 = parseInt(prompt("Give me a number"));
-
-//var number2 = parseInt(prompt("Give me another number"));
-//Function for Addition
 var add = function(number1,number2) {return number1 + number2};
-
-//Function for Subtracting
 var subtract = function(number1,number2) {return number1 - number2};
-
-//Function for Multiplication
 var multiply = function(number1,number2) {return number1 * number2};
-
-//Function for division
 var divide = function(number1,number2) {return number1 / number2};
 
-//var userHeight = parseInt(prompt("How tall are you in inches?"));
-//var userWeight = parseInt(prompt("How much do you weigh?"));
-//Function for BMI
-var bmi = function(height, weight) {return ((weight / (height * height)) * 703);};
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var num1 = parseInt($("#input1").val());
+    var num2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    if (operator==="add") {
+      var result =add(num1, num2)
+    }
+    else if (operator==="subtract") {
+      var result =subtract(num1, num2)
+    }
 
-//var result = bmi(userHeight, userWeight).toFixed(2);
-//var farenheit = parseInt(prompt("provide temperature in Farenheit"));
-var celsius = parseInt(prompt("provide temperature in Celsius"));
+    else if (operator==="multiply") {
+    var result = multiply(num1, num2)
+    }
 
-//var faren_cels = function(faren) {return ((faren - 32)/1.8)};
-var cels_faren = function(cel) {return (cel * 1.8 + 32)};
+    else if (operator==="divide") {
+      var result = divide(num1, num2)
+    }
+    $("#answersection").text(result);
+  });
 
-//var result_c = faren_cels(farenheit);
-var result_f = cels_faren(celsius);
 
-//alert(result_c);
-alert(result_f);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// //var userHeight = parseInt(prompt("How tall are you in inches?"));
+// //var userWeight = parseInt(prompt("How much do you weigh?"));
+// //Function for BMI
+// var bmi = function(height, weight) {return ((weight / (height * height)) * 703);};
+//
+// //var result = bmi(userHeight, userWeight).toFixed(2);
+// //var farenheit = parseInt(prompt("provide temperature in Farenheit"));
+// var celsius = parseInt(prompt("provide temperature in Celsius"));
+//
+// //var faren_cels = function(faren) {return ((faren - 32)/1.8)};
+// var cels_faren = function(cel) {return (cel * 1.8 + 32)};
+//
+// //var result_c = faren_cels(farenheit);
+// var result_f = cels_faren(celsius);
+//
+// //alert(result_c);
+// alert(result_f);
